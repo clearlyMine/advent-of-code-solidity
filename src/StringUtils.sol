555585a856396c821e578a57deb25cfe176652bf
clearly mine <clearlymine@pm.me>
2024-02-12 22:32:14 +0530

    fix: correct reference to Arachnid/solidity-stringutils

--- src/StringUtils.sol
+++ src/StringUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "string-utils/src/strings.sol";
+import "solidity-stringutils/src/strings.sol";
 
 library StringUtils {
   using strings for *;
