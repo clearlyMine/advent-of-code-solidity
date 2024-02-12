8ed1ccd1d61cb7fa6268ca19cca6f602f094b7cf
clearly mine <clearlymine@pm.me>
2024-02-12 15:28:14 +0530

    refactor: reflect changes to utilities and remove unnecessary stuff from Day01

--- test/Day01.t.sol
+++ test/Day01.t.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.23 <0.9.0;
 
-import { PRBTest } from "@prb/test/src/PRBTest.sol";
 import { console2 } from "forge-std/src/console2.sol";
 import { StdCheats } from "forge-std/src/StdCheats.sol";
 import { Test } from "forge-std/src/Test.sol";
-// import { Vm } from "forge-std/src/Vm.sol";
 
 import { Day01 } from "../src/Day01.sol";
 import { StringUtils } from "../src/StringUtils.sol";
@@ -18,7 +16,6 @@ interface IERC20 {
 /// https://book.getfoundry.sh/forge/writing-tests
 contract Day01Test is StdCheats, Test {
   Day01 internal day01;
-  StringUtils stringUtils;
   string path = "./inputs/day01.txt";
   string sample_path = "./inputs/day01_sample.txt";
   uint256[] inputs;
@@ -29,7 +26,6 @@ contract Day01Test is StdCheats, Test {
     assertTrue(vm.exists(path));
     // Instantiate the contract-under-test.
     day01 = new Day01();
-    stringUtils = new StringUtils();
   }
 
   function _parseInput(string storage p) internal returns (uint256[] memory) {
@@ -39,7 +35,7 @@ contract Day01Test is StdCheats, Test {
         break;
       }
 
-      (uint256 num, bool res) = stringUtils.stringToUint256(line);
+      (uint256 num, bool res) = StringUtils.stringToUint256(line);
       assertTrue(res);
       inputs.push(num);
     }
@@ -47,34 +43,34 @@ contract Day01Test is StdCheats, Test {
   }
 
   function test_day01_part_1_sample() public {
-    uint256[] memory i = _parseInput(sample_path);
-    uint256 part1 = day01.solvePart1(i);
+    uint256[] memory _i = _parseInput(sample_path);
+    uint256 _part1_sample = day01.solvePart1(_i);
 
     console2.log("Part 1 sample: part1");
-    assertEq(part1, 7);
+    assertEq(_part1_sample, 7);
   }
 
   function test_day01_part_1() public {
-    uint256[] memory i = _parseInput(path);
-    uint256 part1 = day01.solvePart1(i);
+    uint256[] memory _i = _parseInput(path);
+    uint256 _part1 = day01.solvePart1(_i);
 
     console2.log("Part 1: part1");
-    assertEq(part1, 1722);
+    assertEq(_part1, 1722);
   }
 
   function test_day01_part_2_sample() public {
-    uint256[] memory i = _parseInput(sample_path);
-    uint256 part2 = day01.solvePart2(i);
+    uint256[] memory _i = _parseInput(sample_path);
+    uint256 _part2_sample = day01.solvePart2(_i);
 
     console2.log("Part 2 sample: part2");
-    assertEq(part2, 5);
+    assertEq(_part2_sample, 5);
   }
 
   function test_day01_part_2() public {
-    uint256[] memory i = _parseInput(path);
-    uint256 part2 = day01.solvePart2(i);
+    uint256[] memory _i = _parseInput(path);
+    uint256 _part2 = day01.solvePart2(_i);
 
     console2.log("Part 2: part2");
-    assertEq(part2, 1748);
+    assertEq(_part2, 1748);
   }
 }
